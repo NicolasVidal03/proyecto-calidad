c920f766bbe732ce97b05e63e4fa92d632c4a638
Belencita <beckiar23@gmail.com>
2024-09-28 16:20:05 -0400

    Se eliminaron los issues de la pagina static/assets/main.js, if condicinal, expresion regular y caracteres de espacio innecesario

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -43,14 +43,12 @@
 
     function validate (input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            if ($(input).val().trim().match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) == null) {
                 return false;
             }
         }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
+        else if ($(input).val().trim() == '') {
+            return false; 
         }
     }
 
